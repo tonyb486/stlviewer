90cee71fd0540daf2addd82980d3cf5c5975246d
stormhun <99377108+stormhun@users.noreply.github.com>
2022-04-02 11:26:22 -0700

    Adding Some STLS

--- stlviewer.js
+++ stlviewer.js
@@ -1,12 +1,12 @@
 
-function STLViewerEnable(classname) {
+function STLViewerEnable(classname, stl_name, col) {
     var models = document.getElementsByClassName(classname);
     for (var i = 0; i < models.length; i++) {
-        STLViewer(models[i], models[i].getAttribute("data-src"));
+        STLViewer(models[i], stl_name, col);
     }
 }
 
-function STLViewer(elem, model) {
+function STLViewer(elem, model, color_hx) {
 
     if (!WEBGL.isWebGLAvailable()) {
         elem.appendChild(WEBGL.getWebGLErrorMessage());
@@ -28,20 +28,28 @@ function STLViewer(elem, model) {
     controls.enableDamping = true;
     controls.rotateSpeed = 0.05;
     controls.dampingFactor = 0.1;
-    controls.enableZoom = false;
-    controls.enablePan = false;
-    controls.autoRotate = true;
+    controls.enableZoom = true;
+    controls.enablePan = true;
+    controls.autoRotate = false;
     controls.autoRotateSpeed = .75;
 
     var scene = new THREE.Scene();
 
     scene.add(new THREE.HemisphereLight(0xffffff, 0x080820, 1.5));
 
+    var slide_geo = "slide.stl";
+    (new THREE.STLLoader()).load(slide_geo, function (geometry) {
+        var slide_mat = new THREE.MeshPhongMaterial({ color: 0x00ff00, specular: 100, shininess: 100 });
+        var slide = new THREE.Mesh(geometry, slide_mat);
+        scene.add(slide);
+        slide.position.z += 40;
+        slide.scale.x -= .2;
+        slide.rotation.z += 3.14159;
+    });
     (new THREE.STLLoader()).load(model, function (geometry) {
-        var material = new THREE.MeshPhongMaterial({ color: 0xff5533, specular: 100, shininess: 100 });
+        var material = new THREE.MeshPhongMaterial({ color: color_hx, specular: 100, shininess: 100 });
         var mesh = new THREE.Mesh(geometry, material);
         scene.add(mesh);
-
         // Compute the middle
         var middle = new THREE.Vector3();
         geometry.computeBoundingBox();
@@ -51,6 +59,7 @@ function STLViewer(elem, model) {
         mesh.position.x = -1 * middle.x;
         mesh.position.y = -1 * middle.y;
         mesh.position.z = -1 * middle.z;
+
 
         // Pull the camera away as needed
         var largestDimension = Math.max(geometry.boundingBox.max.x,
